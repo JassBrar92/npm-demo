46cb01d62746922232d9bb26d29743ca6df5c06c
jass <jasbrar080@gmail.com>
2023-10-09 15:10:50 -0700

    adding update version of adding library

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const _=require('underscore');
 const result=_.contains([5,2,8],2);
 console.log(result);
 const addNum=require('adding-library');   // using own publish dependency
-const sum=addNum.add(10,12);
+const sum=addNum.add(19,12);
 console.log(sum);
