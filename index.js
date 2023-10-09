75d47001661c09f4bd18f5de292234f0593f7f8a
jass <jasbrar080@gmail.com>
2023-10-09 14:57:09 -0700

    use the own publish adding-library package on npm

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const _=require('underscore');
 const result=_.contains([5,2,8],2);
 console.log(result);
+const addNum=require('adding-library');   // using own publish dependency
+const sum=addNum.add(10,12);
+console.log(sum);
